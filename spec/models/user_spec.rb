97c862083a68347591cfc0b71382dcb7624de6ee
Lucas Colwell <colwellc37@gmail.com>
2023-07-24 09:58:39 -0600

    Test: user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'relationships' do
+
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password_digest }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should have_secure_password}
+  end
+
+  it "password bcrypt" do
+    user = User.create(username: 'oilguy25', email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq('password123')
+  end
+end
