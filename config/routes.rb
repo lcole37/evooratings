af88f76bf73ab94c4131c064ba1ec83088478af7
Lucas Colwell <colwellc37@gmail.com>
2023-09-10 16:55:08 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'landing_page#index'
+  #
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+  # get '/user', to: 'users#show'
+  #
+  resources :users, only: [:show, :new] #do
+  #   resources :favorites, only: [:index]
+  #   resources :reviews, only: [:index]
+  # end
+  #
+  # get '/user_favorites_index', to: 'users#'
+  delete '/logout', to: "users#logout_user"
+
 end
